aae6819aad2c0a66748f9555041f9bef52393478
leeseulkim <leeseulkim@localhost>
2020-04-01 00:39:19 +0900

    dd

--- java 4th week/src/day17/Ex02_Samgak.java
+++ java 4th week/src/day17/Ex02_Samgak.java
@@ -1,5 +1,43 @@
 package day17;
 
 public class Ex02_Samgak {
+	
+	//삼각형 변수선언
+	private int width, height;
+	private double area;
+	
+	//기본생성자함수
+	public Ex02_Samgak() {}
+	
+	//생성자함수 오버로딩
+	public Ex02_Samgak(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
 
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	
 }
